b4902a6c7182e034dc5a46ab9def3f85ca9438bd
Tanzim Mokammel <tanzim.mokammel@moonfare.com>
2020-12-04 07:52:13 +0100

    update seed

--- seed-bookstore/index.ts
+++ seed-bookstore/index.ts
@@ -11,7 +11,7 @@ export interface Book {
 const uri = process.env.MONGO_URI || "mongodb://localhost:27017";
 const dbName = process.env.DB_NAME || "bookstore";
 const collName = process.env.MONGO_COLL || "books";
-const SIZE = 299;
+const SIZE = 5;
 
 const client = new MongoClient(uri, { useUnifiedTopology: true });
 
