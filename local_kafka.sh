b3d7e574c3af378310237fb568903388a07bc0e5
Tanzim Mokammel <mtanzim@gmail.com>
2020-12-23 21:19:42 -0500

    .

--- local_kafka.sh
+++
@@ -1,5 +0,0 @@
-/home/mokammel/Applications/kafka-2.6.0-src/bin/zookeeper-server-start.sh config/zookeeper.properties &
-/home/mokammel/Applications/kafka-2.6.0-src/bin/kafka-server-start.sh config/server.properties &
-/home/mokammel/Applications/kafka-2.6.0-src/bin/kafka-topics.sh --create --topic cart-payment --bootstrap-server localhost:9092
-/home/mokammel/Applications/kafka-2.6.0-src/bin/kafka-topics.sh --create --topic cart-payment-processed --bootstrap-server localhost:9092
-/home/mokammel/Applications/kafka-2.6.0-src/bin/kafka-topics.sh --create --topic cart-shipment --bootstrap-server localhost:9092
