d5bea2260c3972f91b682ac756332a39c21cfe28
Tanzim Mokammel <mtanzim@gmail.com>
2020-12-25 22:46:05 -0500

    add btn handlers

--- warehouse-client/main.js
+++ warehouse-client/main.js
@@ -5,48 +5,63 @@ async function fetchData() {
   return shipments;
 }
 
+function renderData(rows) {
+  const buttonIds = [];
+  document.getElementById("root").innerHTML = `<div class="grid-container">
+${rows
+  .map((row) => {
+    const {
+      cart: { cartId, items, address, email, phone },
+      shipped,
+      paid,
+    } = row;
+    const curBtnId = `btn-${cartId}`;
+    buttonIds.push(curBtnId);
+    // TODO: do this from the backend
+    if (shipped) {
+      return "";
+    }
+    return `
+      <div id=${cartId} class="grid-item">
+        <h2><code>${cartId}</code>\t${paid ? "Paid" : "Pending"}</h2>
+        <h3>Contact</h3>
+        <p>${email}<p>
+        <p>${phone}<p>
+        <p>${address}<p>
+        <h3>Books</h3>
+        <ul>
+        ${items
+          .map(
+            ({ book, qty }) =>
+              `<li>${book.title} - ${book.author} - ${qty}</li>`
+          )
+          .join("")}
+        </ul>
+        <button ${paid ? "" : "disabled"} id="${curBtnId}">Ship it!</button>
+      </div>
+  `;
+  })
+  .join("")}
+</div>`;
+  return buttonIds;
+}
+
+function attachBtnHandlers(buttonIds) {
+  buttonIds.forEach((btnId) => {
+    document.getElementById(btnId).onclick = () => alert(`${btnId} Clicked`);
+  });
+}
+
 async function main() {
   try {
     document.getElementById("root").innerHTML = `<h4>Loading...</h4>`;
     const rows = await fetchData();
     console.log(JSON.stringify(rows));
-    document.getElementById("root").innerHTML = `<div class="grid-container"> 
-  ${rows
-    .map((row) => {
-      const {
-        cart: { cartId: _id, items, address, email, phone },
-        shipped,
-        paid,
-      } = row;
-      // TODO: do this from the backend
-      if (shipped) {
-        return "";
-      }
-      return `
-        <div id=${_id} class="grid-item">
-          <h2><code>${_id}</code>\t${paid ? "Paid" : "Pending"}</h2>
-          <h3>Contact</h3>
-          <p>${email}<p>
-          <p>${phone}<p>
-          <p>${address}<p>
-          <h3>Books</h3>
-          <ul>
-          ${items
-            .map(
-              ({ book, qty }) =>
-                `<li>${book.title} - ${book.author} - ${qty}</li>`
-            )
-            .join("")}
-          </ul>
-          <button ${paid ? "" : "disabled"} id="btn-${_id}">Ship it!</button>
-        </div>
-    `;
-    })
-    .join("")}
-  </div>`;
+    attachBtnHandlers(renderData(rows));
   } catch (err) {
     console.log(err);
     document.getElementById("root").innerHTML = `<h4>Failed to load data!</h4>`;
+    return;
   }
 }
 
