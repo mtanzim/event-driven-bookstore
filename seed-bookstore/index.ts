d762e87d1b265c3dd3d2c7dad6e258cf0180d101
Tanzim Mokammel <mtanzim@gmail.com>
2020-12-30 16:58:42 -0500

    update seeding script

--- seed-bookstore/index.ts
+++ seed-bookstore/index.ts
@@ -12,7 +12,9 @@ export interface Book {
 const uri = process.env.MONGO_URI || "mongodb://localhost:27017";
 const dbName = process.env.DB_NAME || "bookstore";
 const collName = process.env.MONGO_COLL || "books";
-const SIZE = 15;
+const SIZE = 3;
+const STOCK_RANGE = [10, 50];
+const STAGED_RANGE = [0, 0];
 
 const client = new MongoClient(uri, { useUnifiedTopology: true });
 
@@ -28,12 +30,16 @@ async function seed() {
       console.warn(err);
     }
     // placeholder fake data
+    const [stockMin, stockMax] = STOCK_RANGE;
+    const [stagedMin, stagedMax] = STAGED_RANGE;
     const fakeBooks: Book[] = [...Array(SIZE).keys()].map((_) => ({
       title: faker.random.words(3),
       author: `${faker.name.firstName()} ${faker.name.lastName()}`,
       price: faker.commerce.price(10, 300, 2),
-      stock: Math.floor(faker.random.number({ min: 10, max: 50 })),
-      stagedQty: Math.floor(faker.random.number({ min: 0, max: 9 })),
+      stock: Math.floor(faker.random.number({ min: stockMin, max: stockMax })),
+      stagedQty: Math.floor(
+        faker.random.number({ min: stagedMin, max: stagedMax })
+      ),
     }));
     const res = await coll.insertMany(fakeBooks, { ordered: true });
     console.log(JSON.stringify(fakeBooks, null, 2));
